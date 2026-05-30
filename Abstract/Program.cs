a7b0bede51c7e2b77d96b0cfd7fc536a98d9bb36
ADMIN <ADMIN@H-RUSHI-KESH>
2026-05-30 11:53:11 +0530

    git

---
+++ Abstract/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    abstract class shape
+    {
+        public abstract void ClculateArea();
+         
+    }
+    class circle : shape
+    {
+        public double radius;
+        public circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public override void ClculateArea()
+        {
+            Console.WriteLine("Area of circle is : " + (3.14 * radius * radius));
+        }
+    }
+
+    class rectangle : shape
+    {
+        public double length;
+        public double width;
+        public rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+        public override void ClculateArea()
+        {
+            Console.WriteLine("Area of rectangle is : " + (length * width));
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Static-variables/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_variables
+{
+    class Student
+    {
+        int id;
+        string name; 
+        public static string school = "Bhashyam";
+
+        public Student(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public void Desplay()
+        {
+            Console.WriteLine("Student ID: " + id);
+            Console.WriteLine("Student Name: " + name);
+            Console.WriteLine("School: " + school);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student(22,"John");
+            s1.Desplay();
+            Console.WriteLine("========================");
+            Student.school = "BIIT";
+            s1.Desplay();
+        }
+    }
+}
